5664862acce79468294f2224e94235f0d27e475c
jonkire <erikjonatha.xii@gmail.com>
2025-05-09 22:17:22 -0300

    atividade ls

--- run.js
+++ run.js
@@ -1,14 +1,14 @@
 const vec = [1, 2, 3, 4, 5];
 
 console.log(
-  vec.reduce((addition, x) => addition += x)
+  vec.reduce((sum, x) => sum + x, 0)
 );
 
 console.log(
-  vec.filter((x) => x%2 != 0).reduce((addition, x) => addition+=x)
+  vec.filter((x) => x % 2 != 0).reduce((sum, x) => sum + x, 0)
 );
 
 console.log(
-  vec.reduce((product, x) => product *= x)
+  vec.reduce((product, x) => product * x, 1)
 );
 
